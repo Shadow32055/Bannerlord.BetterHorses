0e3634e2a8ee3c4b91a2caa83b0f6799bc0505a9
Shadow32055 <shadow32055@gmail.com>
2024-01-11 21:23:59 -0500

    Update RefValues.cs

--- BetterHorses/Localizations/RefValues.cs
+++ BetterHorses/Localizations/RefValues.cs
@@ -1,46 +1,46 @@
 ﻿namespace BetterHorses.Localizations {
     public class RefValues {
 
-        public const string RearingText = "Mount Rearing";
-        public const string AdjText = "Mount Adjustments";
-        public const string GodMountText = "Invulnerable Mount";
-        public const string CommandText = "Mount Commands";
-        public const string RegenText = "Regeneration";
-        public const string RestockText = "Restock";
+        public const string RearingText = "{=BHR_v94tx5}Mount Rearing";
+        public const string AdjText = "{=BHR_CCvKIi}Mount Adjustments";
+        public const string GodMountText = "{=BHR_RLGBPF}Invulnerable Mount";
+        public const string CommandText = "{=BHR_XshpXv}Mount Commands";
+        public const string RegenText = "{=BHR_t58FZY}Regeneration";
+        public const string RestockText = "{=BHR_1gFv7J}Restock";
 
-        public const string FollowText = "Horse will follow you.";
-        public const string StayText = "Horse will stay put.";
+        public const string FollowText = "{=BHR_uwB0Hp}Mount will follow you.";
+        public const string StayText = "{=BHR_4fzaol}Mount will stay put.";
 
-        public const string MountRearText = "Mounts Don't Rear";
-        public const string MountRearHint = "Should mounts rear when hit";
-        public const string ChargeText = "Charge Damage";
-        public const string ChargeHint = "Change charge damage by a percent value";
-        public const string SpeedText = "Speed";
-        public const string SpeedHint = "Change speed by a percent value";
-        public const string ManeuverText = "Maneuver";
-        public const string ManeuverHint = "Change maneuver by a percent value";
-        public const string AccelText = "Acceleration";
-        public const string AccelHint = "Change acceleration by a percent value";
-        public const string PlayerText = "Player Only";
-        public const string PlayerHint = "Should only impact the player";
-        public const string InvulnerableText = "Invulnerable Mount";
-        public const string InvulnerableHint = "Should player mount can take damage or not. Only while mounted";
-        public const string MountCommandsText = "Mount Commands";
-        public const string MountCommandsHint = "Should or not to enable mount commands, stay and follow";
-        public const string KeyText = "Key";
-        public const string KeyHint = "What key to use for associated action. Examples 'Q', 'Numpad0'";
-        public const string AllowRegenText = "Allow Mount Regen";
-        public const string AllowRegenHint = "Should the player mount regenerate health over time";
-        public const string RegenAmountText = "Mount Health Regen Amount";
-        public const string RegenAmountHint = "Percentage of health restored each regeneration";
-        public const string RegenIntervalText = "Mount Health Regen Interval";
-        public const string RegenIntervalHint = "Time between each regeneration";
-        public const string RegenDelayText = "Mount Regen Damage Delay";
-        public const string RegenDelayHint = "Time until next heal after damage";
-        public const string AllowRestockText = "Allow restocking";
-        public const string AllowRestockHint = "Should you be able to get ammo back from your mount";
-        public const string RestockTimesText = "Restock times";
-        public const string RestockTimesHint = "How many times can you get ammo from your horse";
+        public const string MountRearText = "{=BHR_lalihN}Mounts Don't Rear";
+        public const string MountRearHint = "{=BHR_KRpAl4}Should mounts rear when hit";
+        public const string ChargeText = "{=BHR_LBgVTB}Charge Damage";
+        public const string ChargeHint = "{=BHR_1UCCxg}Change charge damage by a percent value";
+        public const string SpeedText = "{=BHR_L59iLT}Speed";
+        public const string SpeedHint = "{=BHR_oFVO8r}Change speed by a percent value";
+        public const string ManeuverText = "{=BHR_D3jaWJ}Maneuver";
+        public const string ManeuverHint = "{=BHR_y40DN2}Change maneuver by a percent value";
+        public const string AccelText = "{=BHR_uP2gxB}Acceleration";
+        public const string AccelHint = "{=BHR_bysqbZ}Change acceleration by a percent value";
+        public const string PlayerText = "{=BHR_FBBR4W}Player Only";
+        public const string PlayerHint = "{=BHR_YBWPXK}Should only impact the player";
+        public const string InvulnerableText = "{=BHR_8M69MF}Invulnerable Mount";
+        public const string InvulnerableHint = "{=BHR_oFj0DS}Should player mount can take damage or not. Only while mounted";
+        public const string MountCommandsText = "{=BHR_MsDsIS}Mount Commands";
+        public const string MountCommandsHint = "{=BHR_ta4QFj}Should or not to enable mount commands, stay and follow";
+        public const string KeyText = "{=BHR_KvjKvF}Key";
+        public const string KeyHint = "{=BHR_7xRBer}What key to use for associated action. Examples 'Q', 'Numpad0'";
+        public const string AllowRegenText = "{=BHR_sJR9xp}Allow Mount Regen";
+        public const string AllowRegenHint = "{=BHR_pfJKf2}Should the player mount regenerate health over time";
+        public const string RegenAmountText = "{=BHR_EofUVo}Mount Health Regen Amount";
+        public const string RegenAmountHint = "{=BHR_JDuib4}Percentage of health restored each regeneration";
+        public const string RegenIntervalText = "{=BHR_Qlxprz}Mount Health Regen Interval";
+        public const string RegenIntervalHint = "{=BHR_5ikEoP}Time between each regeneration";
+        public const string RegenDelayText = "{=BHR_UJtVx0}Mount Regen Damage Delay";
+        public const string RegenDelayHint = "{=BHR_M7tpYU}Time until next heal after damage";
+        public const string AllowRestockText = "{=BHR_QpwQU3}Allow restocking";
+        public const string AllowRestockHint = "{=BHR_LK0Y2X}Should you be able to get ammo back from your mount";
+        public const string RestockTimesText = "{=BHR_8GKCt3}Restock times";
+        public const string RestockTimesHint = "{=BHR_SPUG8I}How many times can you get ammo from your horse";
 
 
     }
